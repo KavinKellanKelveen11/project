fcbc45b9eda727bb0ef6fdbdef5fce70118ab5ba
KavinFirstGit <kavin6627@gmail.com>
2023-04-24 17:46:37 +0530

    techshop

---
+++ Back-end/src/main/java/com/springboot/Electronic.java
@@ -0,0 +1,16 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Electronic
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Electronic.class, args);
+		System.out.println("--------------Started-----------");
+	}
+
+}
